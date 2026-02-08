41e605749ab22d10b510b78ebf8bc8568c41dbcd
Kiet Minh Mai <s4163602@student.rmit.edu.au>
2026-02-08 17:13:00 +1000

    Zino commiti

--- trello_django/settings.py
+++ trello_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yk56dp)=42nc4c=mhpf9s_gqrnh=ns#d6$ql$_r$+_rw251(l*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['lini-36.onrender.com']
 
